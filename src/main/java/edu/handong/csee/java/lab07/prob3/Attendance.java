3f5ee5090503dd64189b9b3e7fd060499c1df6ae
SovanChhaya <chhormsovanchhaya@gmail.com>
2018-04-04 11:02:45 +0700

    lab07_prob03 javadoc with comment line

--- src/main/java/edu/handong/csee/java/lab07/prob3/Attendance.java
+++ src/main/java/edu/handong/csee/java/lab07/prob3/Attendance.java
@@ -1,66 +1,85 @@
-package edu.handong.csee.java.lab07.prob3;
-import java.util.*;
+package edu.handong.csee.java.lab07.prob3;// package name of lab07 prob03
+import java.util.*;//import java util package
+
+/**
+ * Random Attendance of student 
+ * No need to input number 
+ * Random Automatically
+ * @author SovanEco(Chhaya)
+ *
+ */
 public class Attendance {
-private String name =" ";
-private int year = 0;
-private String Student_id = " ";
-private int missed = 0;
+	private String name =" ";
+	private int year = 0;
+	private String Student_id = " ";
+	private int missed = 0;
 
-//constructor
-public Attendance () {
-	this.name = "Null";
-	this.year = 0;
-	this.Student_id ="Null";
-	this.missed = 0;
-	
-}
+	//constructor
+	public Attendance () {
+		this.name = "Null";
+		this.year = 0;
+		this.Student_id ="Null";
+		this.missed = 0;
 
-public Attendance (String name , int year , String Student_id, int missed)
-{
-	this.name = name;
-	this.year = year;
-	this.Student_id = Student_id;
-	this.missed = missed;
-	
-}
+	}
 
-//getters
+	public Attendance (String name , int year , String Student_id, int missed)
+	{
+		this.name = name;
+		this.year = year;
+		this.Student_id = Student_id;
+		this.missed = missed;
 
-public String get_name() {
-	
-	return name;
-	
-}
-public int get_year() {
-	return year;
-	
-}
-public String get_id() {
-	return Student_id;
-}
-public int get_missed () {
-	return missed;
-	
-}
-//Setters
+	}
 
-public void set_name(String name) {
-	
-	this.name = name;
-	
-}
-public void set_year(int year) {
-	this.year = year;
-	
-}
-public void set_id(String Student_id) {
-	this.Student_id= Student_id;
-	
-	
-}
-public void set_missed(int missed) {
-	this.missed= missed;
-}
+
+	//getters
+	public String get_name() {
+
+		return name;
+
+	}
+
+	//getters
+	public int get_year() {
+		return year;
+
+	}
+
+	//getters
+	public String get_id() {
+		return Student_id;
+	}
+
+	//getters
+	public int get_missed () {
+		return missed;
+
+	}
+
+	//Setters
+
+	public void set_name(String name) {
+
+		this.name = name;
+
+	}
+	//Setters
+	public void set_year(int year) {
+		this.year = year;
+
+	}
+
+	//Setters
+	public void set_id(String Student_id) {
+		this.Student_id= Student_id;
+
+
+	}
+	//Setters
+	public void set_missed(int missed) {
+		this.missed= missed;
+	}
 
 
 }
--- src/main/java/edu/handong/csee/java/lab07/prob3/Attendance_main.java
+++ src/main/java/edu/handong/csee/java/lab07/prob3/Attendance_main.java
@@ -1,57 +1,91 @@
-package edu.handong.csee.java.lab07.prob3;
-import java.util.Random;
+package edu.handong.csee.java.lab07.prob3;// package name of lab07 prob03
+import java.util.Random;//import Random package for RandomGenerator 
+
+/**
+ * Random Attendance of student 
+ * No need to input number 
+ * Random Automatically
+ * @author SovanEco(Chhaya)
+ *
+ */
 public class Attendance_main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Attendance s_1 = new Attendance();
-		Attendance s_2 = new Attendance();
-		Attendance s_3 = new Attendance("Lucas",1 ,"21833222",0);
-		Attendance s_4 = new Attendance (" Martha ", 2 , " 21733444",0);
-		
+		Attendance s_1 = new Attendance();// object constructor Attendance 
+		Attendance s_2 = new Attendance();// object constructor Attendance 
+		Attendance s_3 = new Attendance("Lucas",1 ,"21833222",0);// object constructor Attendance 
+		Attendance s_4 = new Attendance (" Martha ", 2 , " 21733444",0);// object constructor Attendance 
+
 		s_1.set_name("Jess"); s_1.set_year(4); s_1.set_missed(0);s_1.set_id("21400999");
 		s_2.set_name("kent"); s_2.set_missed(0);s_2.set_id("21700111"); s_2.set_year(2);
-		
-		Random randomGenerator = new Random();
-		
+
+		Random randomGenerator = new Random();// Random constructor  for package
+
 		s_1.set_missed(randomGenerator.nextInt(10));
 		s_2.set_missed(randomGenerator.nextInt(10));
 		s_3.set_missed(randomGenerator.nextInt(10));
 		s_4.set_missed(randomGenerator.nextInt(10));
-		
-		
+
+		//conditional for s_1 
 		if(s_1.get_missed()>6)
 		{
-			System.out.println("I'm sorry" + s_1.get_name()+"  you're fail thi course ");
+			//print out I'm sorry  (value of s_1) you're fail this course
+			System.out.println("I'm sorry" + s_1.get_name()+"  you're fail this course ");
+
+			//print out (value of (s_1 get_name) Number of absence and value of (s_1)
 			System.out .println(s_1.get_name()+"  Number of absence :"+ s_1.get_missed());
 		}
 		else 
+			// print out we'll see about the grade and (s_1.get_name value)
 			System.out .println(" We'll see about the grade"  + s_1.get_name());
-		
+
+
+
+
+		//conditional for s_2 
 		if(s_2.get_missed()>6)
 		{
-			System.out.println("I'm sorry " + s_2.get_name()+"  you're fail thi course");
+			//print out I'm sorry  (value of s_2) you're fail this course
+			System.out.println("I'm sorry " + s_2.get_name()+"  you're fail this course");
+
+			//print out (value of (s_2 get_name) Number of absence and value of (s_2)
 			System.out .println(s_2.get_name()+"  Number of absence :"+ s_2.get_missed());
 		}
 		else 
-			
+			// print out we'll see about the grade and (s_2.get_name value)
 			System.out .println(" We'll see about the grade"  + s_2.get_name());
-		
+
+
+
+
+		//conditional for s_3
 		if(s_3.get_missed()>6)
 		{
-			System.out.println("I'm sorry " + s_3.get_name()+"  you're fail thi course ");
+			//print out I'm sorry  (value of s_3) you're fail this course
+			System.out.println("I'm sorry " + s_3.get_name()+"  you're fail this course ");
+
+			//print out (value of (s_3get_name) Number of absence and value of (s_3)
 			System.out .println(s_3.get_name()+"  Number of absence :"+ s_3.get_missed());
 		}
 		else 
+			// print out we'll see about the grade and (s_3.get_name value)
 			System.out .println(" We'll see about the grade "  + s_3.get_name());
-		
+
+
+
+		//conditional for s_4
 		if(s_4.get_missed()>6)
 		{
-			System.out.println("I'm sorry" + s_4.get_name()+"  you're fail thi course ");
+			//print out I'm sorry  (value of s_4) you're fail this course
+			System.out.println("I'm sorry" + s_4.get_name()+"  you're fail this course ");
+
+			//print out (value of (s_4 get_name) Number of absence and value of (s_4)
 			System.out .println(s_4.get_name()+"  Number of absence :"+ s_4.get_missed());
 		}
 		else 
+			// print out we'll see about the grade and (s_4.get_name value)
 			System.out .println(" We'll see about the grade "  + s_4.get_name());
 	}
 
