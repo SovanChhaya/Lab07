76a1a386d626f6dfdb8d7f1f7b3d1e8a7c14b9b0
SovanChhaya <chhormsovanchhaya@gmail.com>
2018-04-04 11:01:00 +0700

    lab07_prob02 with javadoc and comment line

--- src/main/java/edu/handong/csee/java/lab07/prob2/chicken.java
+++ src/main/java/edu/handong/csee/java/lab07/prob2/chicken.java
@@ -1,49 +1,63 @@
-package edu.handong.csee.java.lab07.prob2;
+package edu.handong.csee.java.lab07.prob2;// package name of lab07 prob02
 
+/**
+ * Fried Chicken menu 
+ * @author SovanEco(Chhaya)
+ *
+ */
 public class chicken {
 	private String name;
 	private double price;
 	private int stars;
-	
-public chicken() {
-	this.name = " ";
-	this.price = 0.0;
-	this.stars= 0;
-}
 
-public chicken(String name, double price ,int stars)
-{
-	this.name = name;
-	this.price = price ;
-	this.stars = stars;
-}
+	// chicken public method
+	public chicken() {
+		this.name = " ";
+		this.price = 0.0;
+		this.stars= 0;
+	}
+	// method and parameter variable 
+	public chicken(String name, double price ,int stars)
+	{
+		this.name = name;
+		this.price = price ;
+		this.stars = stars;
+	}
 
-public String get_name() {
-	return this.name;
-}
+	//get_name String method
+	public String get_name() {
 
-public double get_price() {
-	return this.price;
-	
-}
-public int get_stars() {
-	return this.stars;
-}
+		return this.name;
+	}
 
-public void set_name(String name)
-{
-	this.name = name;
-}
-public void set_price(double price)
-{
-	this.price = price;
-}
-public void set_stars(int stars)
-{
-	this.stars= stars;
-}
+	//get_price double method
+	public double get_price() {
+		return this.price;
+
+	}
+	//get_stars int method
+	public int get_stars() {
+		return this.stars;
+	}
+	//set_name public void no return value and parameter variable as string
+	public void set_name(String name)
+	{
+		this.name = name;
+	}
+
+	//set_name public void no return value and parameter variable as double
+	public void set_price(double price)
+	{
+		this.price = price;
+	}
+
+	//set_name public void no return value and parameter variable as int
+	public void set_stars(int stars)
+	{
+		this.stars= stars;
+	}
 
 
-	
+
 
 }
--- src/main/java/edu/handong/csee/java/lab07/prob2/chicken_main.java
+++ src/main/java/edu/handong/csee/java/lab07/prob2/chicken_main.java
@@ -1,22 +1,27 @@
-package edu.handong.csee.java.lab07.prob2;
-
+package edu.handong.csee.java.lab07.prob2;// package name of lab07 prob02
+/**
+ * Fried Chicken menu 
+ * print out any menu 
+ * @author SovanEco(Chhaya)
+ *
+ */
 public class chicken_main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-chicken menu1 = new chicken("cheese_mustard" , 16000.00,5);
-chicken menu2 = new chicken("Honey_mustard" , 16000.00,5);
-chicken menu3 = new chicken("Spicy_chicken" , 16000.00,1);
+		chicken menu1 = new chicken("cheese_mustard" , 16000.00,5);//constructor
+		chicken menu2 = new chicken("Honey_mustard" , 16000.00,5);//constructor
+		chicken menu3 = new chicken("Spicy_chicken" , 16000.00,1);//constructor
 
-	menu1.set_stars(3);
-	menu2.set_stars(4);
-	menu3.set_stars(1);
-	
-	System.out.println(menu1.get_name()+" 's rating is "+ menu1.get_stars());
-	System.out.println(menu2.get_name()+" 's rating is "+ menu2.get_stars());
-	System.out.println(menu3.get_name()+" 's rating is "+ menu3.get_stars());
-	
-		
+		menu1.set_stars(3);// menu1 as object constructor set_stars method
+		menu2.set_stars(4);// menu2 as object constructor set_stars method
+		menu3.set_stars(1);// menu3 as object constructor set_stars method
+
+		System.out.println(menu1.get_name()+" 's rating is "+ menu1.get_stars());// print out (value of num1) 's rating is (value)
+		System.out.println(menu2.get_name()+" 's rating is "+ menu2.get_stars());// print out (value of num2) 's rating is (value)
+		System.out.println(menu3.get_name()+" 's rating is "+ menu3.get_stars());// print out (value of num3) 's rating is (value)
+
+
 	}
 
 }
